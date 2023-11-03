f9799f9de99735f6b3263b98af430e064226eb2d
Sebastian Smiley <sebastianswms@gmail.com>
2023-11-03 13:47:41 -0400

    Match up data between API requests

--- testapp/renderer.js
+++ testapp/renderer.js
@@ -4,13 +4,33 @@ const resultDiv = document.getElementById('result');
 apiButton.addEventListener('click', async () => {
   const year = document.getElementById('apiRequestYear').value;
   const eventCode = document.getElementById('apiRequestEvent').value;
-  const match = document.getElementById('apiRequestMatch').value;
+
+  let schedule;
+  let scores;
+  let scoresMap = new Map();
+
   try {
-    url = `https://frc-api.firstinspires.org/v3.0/${year}/scores/${eventCode}/Qualification?matchNumber=${match}`;
-    const response = await window.api.get(url);
-    resultDiv.innerHTML = JSON.stringify(response, null, 2);
+    url = `https://frc-api.firstinspires.org/v3.0/${year}/scores/${eventCode}/Qualification`;
+    scores = await window.api.get(url);
+    url = `https://frc-api.firstinspires.org/v3.0/${year}/schedule/${eventCode}?tournamentLevel=Qualification`;
+    schedule = await window.api.get(url);
   } catch (error) {
     resultDiv.innerHTML = `Error: ${error.message}`;
   }
+
+
+  for(scoreEntry of scores["MatchScores"]) {
+    scoresMap.set(scoreEntry["matchLevel"] + scoreEntry["matchNumber"], scoreEntry["alliances"])
+  }
+
+  for(scheduleEntry of schedule["Schedule"]) {
+    const mapKey = scheduleEntry["tournamentLevel"] + scheduleEntry["matchNumber"]
+    if (scoresMap.has(mapKey)) {
+      scheduleEntry.alliances = scoresMap.get(scheduleEntry["tournamentLevel"] + scheduleEntry["matchNumber"])
+    }
+  }
+
+  resultDiv.innerHTML = JSON.stringify(schedule, null, 2);
+
 });
 
