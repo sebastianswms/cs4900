9603e689a70cb3878c661867fe44869b364b8b3c
Hoken <Hoken8@users.noreply.github.com>
2023-11-20 16:53:36 -0500

    removed: redundant code

--- code/scouting-hub/src/components/charts/AlliancePie.js
+++ code/scouting-hub/src/components/charts/AlliancePie.js
@@ -5,60 +5,6 @@ import ChartDataLabels from "chartjs-plugin-datalabels";
 
 ChartJS.register(ArcElement, Tooltip, Legend, Title, ChartDataLabels);
 
-export const data = {
-  labels: ["4779", "2054", "5675"],
-  datasets: [
-    {
-      // label: "OPR",
-      data: [31.6, 33.5, 34.9],
-      backgroundColor: [
-        "rgba(255, 99, 132, 0.2)",
-        "rgba(54, 162, 235, 0.2)",
-        "rgba(255, 206, 86, 0.2)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-      ],
-      borderWidth: 1,
-    },
-  ],
-};
-
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
-      labels: {
-        boxWidth: 20,
-        font: {
-          size: 20,
-        },
-      },
-    },
-    title: {
-      display: true,
-      text: "Red Alliance",
-      font: {
-        size: 30,
-      },
-    },
-    datalabels: {
-      formatter: function (value) {
-        return `${value} %`;
-      },
-      font: {
-        size: 30,
-      },
-    },
-    tooltip: {
-      enabled: false,
-    },
-  },
-};
-
 export default function AlliancePie({ title, teams, values }) {
   const data = {
     labels: teams,
