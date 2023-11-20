46cfa490eedaf76f9e0a690691e6d1747c125a78
Hoken <Hoken8@users.noreply.github.com>
2023-11-20 15:07:01 -0500

    added: csv render input form

--- code/scouting-hub/src/pages/CSVDataPage/index.js
+++ code/scouting-hub/src/pages/CSVDataPage/index.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 
 export default function CSVDataPage() {
+  const [myfile, setMyFile] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(myfile);
+  };
   return (
     <div>
       <Navbar />
       <h1>CSV Data</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          id="myFile"
+          name="myFile"
+          onChange={(e) => setMyFile(e.target.value)}
+        ></input>
+        <br />
+        <button type="submit">Read CVS Data</button>
+      </form>
+      <p>{myfile}</p>
     </div>
   );
 }
