d74504ef2c0c1c050a96339dd15be471d63719ef
Hoken <Hoken8@users.noreply.github.com>
2023-12-08 13:44:20 -0500

    removed: extra files

--- testapp/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  get: async (url, config) => {
-    const response = await ipcRenderer.invoke('api-get', { url, config });
-    return response;
-  }
-});
-
