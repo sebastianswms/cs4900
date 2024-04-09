c74b953a0e8a42c0b277f5e882275e4ba1f5ecdf
RisaWoods <cullen.armstrong10@gmail.com>
2024-04-09 18:10:13 -0400

    Update: Added database and layout importing as well as data parsing. currently not functioning

--- code/scouting-hub/src/pages/AllianceSelectionPage/index.js
+++ code/scouting-hub/src/pages/AllianceSelectionPage/index.js
@@ -1,16 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import "./index.css";
 import SortableTable from "../../components/tables/sortableTable";
 import TeamSortableTable from "../../components/tables/teamSortableTable";
 import AllianceSelectionForm from "../../components/forms/allianceSelectionForm";
 import AllianceSelectionDisplay from "../../components/tables/allianceSelectionDisplay";
-import { teamData as importedTeamData, labels } from "../../data/teamData";
 
 export default function AllianceSelectionPage() {
-  const [teamData, setTeamData] = useState(importedTeamData);
+  const [teamData, setTeamData] = useState([]);
   const [optionsArray, setOptionsArray] = useState([]);
-  
+  const [layout, setLayout] = useState(null);
+  const [settings, setSettings] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      
+      await window.evnConfig.readConfig().then((data)=> {
+            console.log(data); 
+            setSettings(data)
+      });
+
+      await window.database.findByObject("layouts", { name: settings.LAYOUT }.then((layout)=> {
+            console.log(layout);
+            const parsedLayout = parseLayout(layout);
+            setLayout(parsedLayout);
+      }));
+      
+      await window.database.findByObject("scores", { layoutID: layout }.then((scores)=> {
+            console.log(scores);
+            const parsedScores = parseScores(scores);
+            setTeamData(parsedScores);
+
+      }));
+
+      const teamData = parseTeamData(teamData, layout);
+      
+    }
+
+    fetchData();
+  }, []);
+
   const sortTable = (group, index) => {
     const sorted = teamData.toSorted((a, b) => {
       const valA = a[group]["data"][index];
@@ -35,6 +64,62 @@ export default function AllianceSelectionPage() {
     setTeamData(sorted);
   };
 
+  function parseLayout(layout) {
+    layout.categories = JSON.parse(layout.categories);
+
+    for (let i = 0; i < 5; i++) {
+        const subCategoryKey = `subCategories${i}`;
+        if (layout.hasOwnProperty(subCategoryKey) && layout[subCategoryKey]) {
+            layout[subCategoryKey] = JSON.parse(layout[subCategoryKey]);
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const headerKey = `headers${i}`;
+        if (layout.hasOwnProperty(headerKey) && layout[headerKey]) {
+            layout[headerKey] = JSON.parse(layout[headerKey]);
+        }
+    }
+
+    return layout;
+}
+
+function parseScores(scores) {
+  return scores.map(score => {
+      Object.keys(score).forEach(key => {
+          if (Array.isArray(score[key]) && typeof score[key][0] === 'string') {
+              score[key] = score[key].map(value => JSON.parse(value));
+          }
+      });
+      return score;
+  });
+}
+
+function parseTeamData(parsedScores, parsedLayout) {
+  return parsedScores.map(score => {
+      const teamData = {};
+      
+      Object.keys(parsedLayout).forEach(key => {
+          if (key !== 'tableName' && key !== 'columns') {
+              teamData[key] = parsedLayout[key];
+          }
+      });
+      
+      parsedLayout.columns.forEach(column => {
+          if (column.name.startsWith('data')) {
+              const groupIndex = parseInt(column.name.substring(4));
+              const groupLabel = parsedLayout.categories[groupIndex];
+              const groupData = score[column.name];
+              
+              teamData[`group${groupIndex}Label`] = groupLabel;
+              teamData[`group${groupIndex}Data`] = { labels: groupData.labels, data: groupData.data };
+          }
+      });
+      
+      return teamData;
+  });
+}
+
   return (
     <div className="page">
       <div>
@@ -42,46 +127,46 @@ export default function AllianceSelectionPage() {
       </div>
       <div class="page-container">
         <div class="container">
-        <div class="header">
+          <div class="header">
             <h1>Alliance Selection</h1>
           </div>
         </div>
-          <div class="team-table-container">
-            <TeamSortableTable
-              teamData={teamData}
-              group={"group0Data"}
-              category={labels[0]}
-              sortTable={sortTable}
-              nameSort={nameSortTable}
-            />
-            <SortableTable
-              teamData={teamData}
-              group={"group1Data"}
-              category={labels[1]}
-              sortTable={sortTable}
-            />
-            <SortableTable
-              teamData={teamData}
-              group={"group2Data"}
-              category={labels[2]}
-              sortTable={sortTable}
-            />
-            <SortableTable
-              teamData={teamData}
-              group={"group3Data"}
-              category={labels[3]}
-              sortTable={sortTable}
-            />
-            <SortableTable
-              teamData={teamData}
-              group={"group3Data"}
-              category={labels[4]}
-              sortTable={sortTable}
-            />
-          </div>
-          <div className="print-button-container">
-              <button className="print-button" onClick={() => window.print()}>Print</button>
-          </div>
+        <div class="team-table-container">
+          <TeamSortableTable
+            teamData={teamData}
+            group={"group0Data"}
+            category={layout.categories[0]}
+            sortTable={sortTable}
+            nameSort={nameSortTable}
+          />
+          <SortableTable
+            teamData={teamData}
+            group={"group1Data"}
+            category={layout.categories[1]}
+            sortTable={sortTable}
+          />
+          <SortableTable
+            teamData={teamData}
+            group={"group2Data"}
+            category={layout.categories[2]}
+            sortTable={sortTable}
+          />
+          <SortableTable
+            teamData={teamData}
+            group={"group3Data"}
+            category={layout.categories[3]}
+            sortTable={sortTable}
+          />
+          <SortableTable
+            teamData={teamData}
+            group={"group3Data"}
+            category={layout.categories[4]}
+            sortTable={sortTable}
+          />
+        </div>
+        <div className="print-button-container">
+          <button className="print-button" onClick={() => window.print()}>Print</button>
+        </div>
         <div class="container" style={{ minHeight: '160px' }}>
           <div className="header">
             <h2>Alliance Builder</h2>
@@ -89,23 +174,24 @@ export default function AllianceSelectionPage() {
           <div class="alliance-builder-container">
             <div class="center">
               <div className="forms-container">
-                  <AllianceSelectionForm
-                    teamData={teamData}
-                    optionsArray={optionsArray}
-                    setOptionsArray={setOptionsArray}
-                  />
-                </div>
+                <AllianceSelectionForm
+                  teamData={teamData}
+                  optionsArray={optionsArray}
+                  setOptionsArray={setOptionsArray}
+                />
+              </div>
             </div>
             <div class="center">
               <div className="alliance-container">
-                  <AllianceSelectionDisplay 
-                    optionsArray={optionsArray} 
-                    setOptionsArray={setOptionsArray} />
-                </div>
+                <AllianceSelectionDisplay
+                  optionsArray={optionsArray}
+                  setOptionsArray={setOptionsArray}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>  
+      </div>
     </div>
   );
 }
